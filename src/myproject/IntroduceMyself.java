66422ff60ceb0cbcfb4f4f5bce97f4b41bb62393
renta11 <rentabando1322@gmail.com>
2020-03-30 17:53:03 +0900

    新規作成

---
+++ src/myproject/IntroduceMyself.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package myproject;
+
+/**
+ * 自己紹介
+ * @author rentabando
+ *
+ */
+public class IntroduceMyself {
+
+	/**
+	 * メインメソッド
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		String name = new String ("阪東　蓮太");
+//		System.out.println("私の名前は"+name+"です");
+//
+//		int age = 19;
+//		age = age/5;
+//
+//		System.out.println("年齢は"+age+"歳です");
+//		double eyesight =  0.1;
+//		System.out.println("視力は"+eyesight+"です。");
+//
+//		System.out.println("\"");
+
+
+	}
+
+}
